ed14bd13e403a479e6663ec41e6498412ba11e75
lukebates86 <166739697+lukebates86@users.noreply.github.com>
2024-04-14 12:47:48 +0000

    commit

--- chatbot-start/lib/firebase/firebase-config.js
+++ chatbot-start/lib/firebase/firebase-config.js
@@ -1,25 +1,7 @@
-/**
- * Copyright 2023 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// Get your configuration here: https://console.firebase.google.com/project/_/settings/general/web:
-
 export default {
-	projectId: "demo-codelab-chatbot",
-	appId: "demo-codelab-chatbot",
-	apiKey: "demo-codelab-chatbot",
-	storageBucket: "demo-codelab-chatbot.appspot.com",
-	authDomain: "demo-codelab-chatbot.firebaseapp.com",
-};
+    apiKey: "AIzaSyDEpJXCKHGwfq8GyPFo0vLBexbyArFr_i8",
+    authDomain: "chatbot-a61c2.firebaseapp.com",
+    projectId: "chatbot-a61c2",
+    storageBucket: "chatbot-a61c2.appspot.com",
+    appId: "1:760314543301:web:cb54b43de6d650af9744ac",
+  };
--- chatbot-start/next.config.js
+++ chatbot-start/next.config.js
@@ -33,3 +33,28 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// Get your configuration here: https://console.firebase.google.com/project/_/settings/general/web:
+export default {
+	apiKey: "AIzaSyDEpJXCKHGwfq8GyPFo0vLBexbyArFr_i8",
+	authDomain: "chatbot-a61c2.firebaseapp.com",
+	projectId: "chatbot-a61c2",
+	storageBucket: "chatbot-a61c2.appspot.com",
+	messagingSenderId: "760314543301",
+	appId: "1:760314543301:web:cb54b43de6d650af9744ac",
+	measurementId: "G-28KBVX3Q3J"
+};
